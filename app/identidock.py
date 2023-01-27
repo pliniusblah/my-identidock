d23b2fa8254a1b7dffab1ddc45f173444589491e
plinius <plinius@localhost.localdomain>
2023-01-27 18:09:20 -0300

    Replace name

--- app/identidock.py
+++ app/identidock.py
@@ -7,7 +7,7 @@ import html
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
-default_name = 'Joe Bloggs'
+default_name = 'Plinius Blah'
 
 
 @app.route('/', methods=['GET', 'POST'])
